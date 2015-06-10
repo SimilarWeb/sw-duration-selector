95b4dde952fc5b42b4b6efa5625fa1d6cd95e71d
Vlad Sternbach <vlads@similargroup.com>
2015-06-10 21:45:10 +0300

    another styling fix

--- dist/sw-duration-selector.js
+++ dist/sw-duration-selector.js
@@ -198,7 +198,7 @@ angular.module('sw.durationSelector').run(['$templateCache', function($templateC
 
   $templateCache.put('src/duration-selector.html',
     "<div class=\"durationSelector {{options.cssClass}}\">\n" +
-    "    <span class=\"durationSelector-input\" ng-click=\"showPresets = !showPresets\">{{model.displayText}}</span>\n" +
+    "    <span class=\"durationSelector-text\" ng-class=\"{'is-active': showPresets}\" ng-click=\"showPresets = !showPresets\">{{model.displayText}}</span>\n" +
     "    <div class=\"durationSelector-popup\" ng-class=\"{custom:showCustom}\" ng-show=\"showPresets\">\n" +
     "        <div class=\"durationSelector-customPicker\" ng-show=\"showPresets && showCustom\">\n" +
     "            <month-picker class=\"left\" type=\"start\" min-date=\"minDate\" max-date=\"maxDate\"></month-picker>\n" +
